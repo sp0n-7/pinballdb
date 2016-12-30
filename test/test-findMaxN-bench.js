6db437b5edff19c9f5da9761e98256a4e8cd619d
Mark Essel <messel@gmail.com>
2016-12-30 16:16:30 -0500

    test is working again, focusing on time

--- test/test-findMaxN-bench.js
+++ test/test-findMaxN-bench.js
@@ -8,6 +8,7 @@ const fThres  = 0;
 const NMaxDim = 500;
 let   id      = 0;
 
+
 const t0 = Date.now();
 // load bench
 for (let i=0;i < NArrays;i++) {
@@ -16,8 +17,10 @@ for (let i=0;i < NArrays;i++) {
   if (fRand > fThres) {
     const NMembers = Math.random() * NMaxDim + 1;
     let aArray = [];
+    const tStart = Date.now();
     for (let j=0;j < NMembers;j++) {
-      aArray.push({ id: '-k'+id++, score: Date.now() + Math.random() })
+      const oDoc = { id: '-k'+id++, score: tStart + j };
+      aArray.push(oDoc);
     }
     aArraySet.push(aArray);
   }
@@ -31,35 +34,45 @@ for (let i=0;i < NArrays;i++) {
 const t1 = Date.now();
 console.log('loaded test data',t1-t0 + 'ms');
 
-for (let i =0; i < 100;i++) {
+for (let i =0; i < 1;i++) {
   const t2 = Date.now();
-  let aResults = findMaxN({ aArraySet: aArraySet, N: 20 });
-  console.log(Date.now()-t2 + 'ms');
-  // console.log('aResults',aResults,'time',Date.now()-t1 + 'ms');
+  let aRandomSubset = [];
+  for (let j=0;j < NArrays;j++) {
+    const fRand = Math.random();
+    if (fRand > -1) {
+      aRandomSubset.push(aArraySet[j]);
+    }
+  }
+  let aResults = findMaxN({ aArraySet: aRandomSubset, N: 20 });
+  // console.log(Date.now()-t2);
+  console.log('time',Date.now()-t1);
+  // console.log('aResults',aResults);
 }
 
 
 
 // ok performance? 10k arrays, each of random length up to 501
-// messel@messels-MBP:~/Desktop/Dropbox/code/js/db_tuts/pinball_tut/test$ node test-findMaxN-bench.js 
-// loaded test data 1264ms
-// aResults [ { id: '-k2531205', score: 1483110583799.8884 },
-//   { id: '-k2531887', score: 1483110583799.8337 },
-//   { id: '-k2531553', score: 1483110583799.222 },
-//   { id: '-k2531552', score: 1483110583799.7883 },
-//   { id: '-k2531204', score: 1483110583799.181 },
-//   { id: '-k2531203', score: 1483110583799.484 },
-//   { id: '-k2531202', score: 1483110583799.6582 },
-//   { id: '-k2531201', score: 1483110583799.9253 },
-//   { id: '-k2531200', score: 1483110583799.5496 },
-//   { id: '-k2531199', score: 1483110583799.6528 },
-//   { id: '-k2531198', score: 1483110583799.5276 },
-//   { id: '-k2531197', score: 1483110583799.5483 },
-//   { id: '-k2531196', score: 1483110583799.765 },
-//   { id: '-k2531195', score: 1483110583799.3804 },
-//   { id: '-k2531194', score: 1483110583799.5598 },
-//   { id: '-k2531193', score: 1483110583799.4592 },
-//   { id: '-k2531192', score: 1483110583799.5671 },
-//   { id: '-k2531191', score: 1483110583799.1829 },
-//   { id: '-k2531190', score: 1483110583799.048 },
-//   { id: '-k2531189', score: 1483110583799.9211 } ] time 51ms
+// loaded test data 980ms
+// init 4.210931
+// d1 5.122258000000001 d2 0.119447
+// time 48ms
+// aResults [ { id: '-k2491446', score: 1483130099998 },
+//   { id: '-k2487098', score: 1483130099997 },
+//   { id: '-k2491445', score: 1483130099997 },
+//   { id: '-k2487097', score: 1483130099996 },
+//   { id: '-k2491444', score: 1483130099996 },
+//   { id: '-k2487096', score: 1483130099995 },
+//   { id: '-k2491443', score: 1483130099995 },
+//   { id: '-k2487095', score: 1483130099994 },
+//   { id: '-k2491442', score: 1483130099994 },
+//   { id: '-k2479357', score: 1483130099993 },
+//   { id: '-k2487094', score: 1483130099993 },
+//   { id: '-k2491441', score: 1483130099993 },
+//   { id: '-k2479356', score: 1483130099992 },
+//   { id: '-k2487093', score: 1483130099992 },
+//   { id: '-k2491440', score: 1483130099992 },
+//   { id: '-k2479355', score: 1483130099991 },
+//   { id: '-k2487092', score: 1483130099991 },
+//   { id: '-k2491439', score: 1483130099991 },
+//   { id: '-k2479354', score: 1483130099990 },
+//   { id: '-k2487091', score: 1483130099990 } ]
