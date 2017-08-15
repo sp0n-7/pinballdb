cbc0e83efee6bbaac7c517b04f5e8a9efa2bb85e
David Tsang <davidytsang@gmail.com>
2017-08-15 00:18:10 -0400

    add trending to index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   Pinball  : require('./lib/pinball'),
   Pinballs : require('./lib/pinballs'),
-  Trending : require('./lib.trending'),
+  Trending : require('./lib/trending'),
   cache    : require('./lib/cache')
 }
