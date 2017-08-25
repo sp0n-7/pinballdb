e0373907c2b5e6e48fd757a6900a5e496fbc2165
Mark Essel <messel@gmail.com>
2017-08-25 12:40:42 -0400

    work in progress on batchGetFromCacheByScore

--- test/test-cache.js
+++ test/test-cache.js
@@ -174,6 +174,8 @@ subscribeToCache(oSubscriberOptions).then( sub => {
   let aPromises = [];
 
   const t0 = Date.now();
+
+  let midTime = null
   
   for (let i = 0; i < N;i++) {
     // const a = { id: 'a'+i, yoo: 'hoo', isGood: false, and: 100, isGreaterThan: 99.9 };
@@ -183,6 +185,9 @@ subscribeToCache(oSubscriberOptions).then( sub => {
     const oItemBase = Object.assign({}, oIncidentBase);
     const cs = t0 - 10 * 60 * 1000 + i;
     const ts = cs + Math.floor(Math.random() * 60000);
+    if (i === N/2) {
+      midTime = cs
+    }
     const level = Math.floor(Math.random() * 4);
     const oItem = Object.assign(oItemBase, {
       id          : id,
@@ -243,8 +248,13 @@ subscribeToCache(oSubscriberOptions).then( sub => {
 
   const delayAfterDone = 500;
 
+  const setKey = pub.getSortedSetName(cityCode);
   Promise.all(aPromises)
   .then( () => {
+    return pub.batchGetFromCacheByScore({ setKey: setKey, scoreStart: midTime });
+  })
+  .then( aData => {
+    console.log('aData',aData);
     return pub.keys({ pattern: `pb:*`});
   })
   .then( aCacheIds => {
