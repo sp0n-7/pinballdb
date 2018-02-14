4c629d00dbc3d6c8526adb008737e2c4f63c7c12
WKM <wkm@citizen.com>
2018-02-14 15:15:40 -0500

    raven

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var Raven = require('raven');
+Raven.config('https://b0b16c1f2fb7401693eab6bc02d1b7e4:340d8ef84c8c4bef9a1a158777e8187e@sentry.ops.sp0n.io/8').install();
+
+
 module.exports = {
   Pinball  : require('./lib/pinball'),
   Pinballs : require('./lib/pinballs'),
